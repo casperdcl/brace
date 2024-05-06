83e9dcf04a1d174b335df158de2d4f6411d94830
Casper da Costa-Luis <casper.dcl@physics.org>
2024-05-06 11:04:34 +0000

    submit button

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from textwrap import dedent
 
 import requests
 import streamlit as st
@@ -17,25 +16,25 @@ with st.sidebar:
     k = st.slider("Maximum number of relevant chapters for *basic chapter selection*", 1, 50, 10, 1)
     if chapter_filter := st.text_input("Chapter selection (override)", help="regex, e.g. ^(Genesis [12]|Exodus 2)$"):
         pass
-    elif st.checkbox("Use AI-based *chapter selection* (slow, ~5min)"):
+    elif st.checkbox("Use AI-based *chapter selection*"):
         chapter_filter = 'LLM'
     else:
         chapter_filter = ''
 
-if user_question := st.text_area(
-    "Enter your query (try to use complete sentences):",
-    help=dedent("""\
-    e.g.:
+query = st.text_area(
+    "Enter your query (try to use complete sentences):", help="""e.g:
     Define marriage and the relationship between husband (man) and wife (woman).
     What is the difference between faith and works, and can we be saved by faith alone?
-    How should criminals and evil-doers be handled?
-    Explain the Holy Trinity.""")
-):
+    Are sacred tradition and sacred scripture equally important, or is scripture more important?
+    How should criminals and evil-doers be treated and should we punish them?
+    Explain the Holy Trinity, and how can one God exist in three persons?""")
+submit = st.button("Submit")
+if query and submit:
     with st.spinner("Searching for answers in the Bible..."):
         pbar = st.progress(0)
         res = requests.get(
             os.getenv('BRACE_BACKEND_URL', 'http://localhost:8090/api'), stream=True,
-            params={'q': user_question, 'chapter_filter': chapter_filter, 'max_chapters': k})
+            params={'q': query, 'chapter_filter': chapter_filter, 'max_chapters': k})
         for chunk in res.iter_content(None, True):
             if "*basic chapter selection*" in chunk:
                 pbar.progress(5)
