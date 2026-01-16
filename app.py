114e7203481ed307c11b1d6cfdec14194ce4f17e
Casper da Costa-Luis <casper.dcl@physics.org>
2026-01-16 16:15:57 +0000

    update favicon URLs

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CNAME = 'brace.cdcl.ml'
 BRACE_BACKEND_URL = os.getenv('BRACE_BACKEND_URL', 'http://localhost:8090')
 st.set_page_config(
     page_title="BRACE — Bible retrieval-augmented (Catholic edition)",
-    page_icon='https://raw.githubusercontent.com/casperdcl/brace/main/favicon.svg',
+    page_icon='https://img.cdcl.ml/brace.svg',
     initial_sidebar_state='collapsed',
     menu_items={'Get help': 'mailto:brace@cdcl.ml',
                 'Report a bug': 'https://github.com/casperdcl/brace/issues/new',
